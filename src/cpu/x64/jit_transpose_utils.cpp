792045cc0d70ad8be4411e30780e7a60cf5842cb
Emilio Cota <ecg@google.com>
2022-02-28 17:06:14 -0500

    cpu: x64: pass kernel names to Xbyak::CodeGenerator
    By wiring them through jit_generator's constructor.
    
    This allows profilers such as perf(1) to correctly
    label code from jit'ed kernels. For example:
    
    $ perf record -o /tmp/perf.data -- make -j test
    
    - Before this patch:
    $ perf report -i /tmp/perf.data --stdio | egrep '(Overhead|memfd)' | head -6
    Overhead    Command          Shared Object                Symbol
         0.05%  cnn-inference-f  memfd:xbyak (deleted)        [.] 0x00000000000000ba
         0.04%  test_eltwise     memfd:xbyak (deleted)        [.] 0x0000000000000055
         0.04%  cnn-inference-f  memfd:xbyak (deleted)        [.] 0x00000000000000cc
         0.04%  cnn-inference-f  memfd:xbyak (deleted)        [.] 0x00000000000000e0
         0.04%  cnn-inference-f  memfd:xbyak (deleted)        [.] 0x00000000000000f4
    
    - After:
    $ perf report -i /tmp/perf.data --stdio | egrep '(Overhead|oneDNN) | head -6
    Overhead    Command          Shared Object                                   Symbol
         0.05%  test_eltwise     oneDNN:jit_avx512_core_cvt_ps_to_bf16 (deleted) [.] 0x0000000000000055
         0.04%  cnn-inference-f  oneDNN:jit_avx_gemv_t_f32_kern (deleted)        [.] 0x00000000000000ba
         0.04%  cnn-inference-f  oneDNN:jit_avx_gemv_t_f32_kern (deleted)        [.] 0x00000000000000e0
         0.04%  cnn-inference-f  oneDNN:jit_avx_gemv_t_f32_kern (deleted)        [.] 0x00000000000000cc
         0.04%  cnn-inference-f  oneDNN:jit_avx_gemv_t_f32_kern (deleted)        [.] 0x00000000000000f4
    
    Note: this patch is quite mechanical so I used a script to
    edit most files (+clang-format). With another script I verified
    that at least one constructor from each class with an invoked
    DECLARE_CPU_JIT_AUX_FUNCTIONS macro does use jit_name(), and
    inspected the few classes with more than one constructor. This
    means that all classes with a DECLARE_CPU_JIT_AUX_FUNCTIONS
    invocation should now get an appropriately-named memory region.

--- src/cpu/x64/jit_transpose_utils.cpp
+++ src/cpu/x64/jit_transpose_utils.cpp
@@ -36,7 +36,8 @@ using namespace Xbyak;
 struct jit_trans_iw_ic_t : public jit_trans_src_t, public jit_generator {
     DECLARE_CPU_JIT_AUX_FUNCTIONS(jit_trans_iw_ic_t)
 
-    jit_trans_iw_ic_t(const jit_conv_conf_t *conf) : jit_trans_src_t(conf) {}
+    jit_trans_iw_ic_t(const jit_conv_conf_t *conf)
+        : jit_trans_src_t(conf), jit_generator(jit_name()) {}
 
     void operator()(ctx_t *ctx) override { jit_generator::operator()(ctx); }
 
@@ -340,7 +341,7 @@ void jit_trans_iw_ic_t::generate() {
 struct jit_trans_iw_ic_int16_t : public jit_trans_src_t, public jit_generator {
     DECLARE_CPU_JIT_AUX_FUNCTIONS(jit_trans_iw_ic_int16_t)
     jit_trans_iw_ic_int16_t(const jit_conv_conf_t *conf)
-        : jit_trans_src_t(conf) {}
+        : jit_trans_src_t(conf), jit_generator(jit_name()) {}
 
     void operator()(ctx_t *ctx) override { jit_generator::operator()(ctx); }
 
@@ -777,7 +778,8 @@ void jit_trans_iw_ic_int16_t::generate() {
 
 struct jit_trans_ow_oc_t : public jit_trans_dst_t, public jit_generator {
     DECLARE_CPU_JIT_AUX_FUNCTIONS(jit_trans_ow_oc_t)
-    jit_trans_ow_oc_t(const jit_conv_conf_t *conf) : jit_trans_dst_t(conf) {}
+    jit_trans_ow_oc_t(const jit_conv_conf_t *conf)
+        : jit_trans_dst_t(conf), jit_generator(jit_name()) {}
 
     void operator()(ctx_t *ctx) override { jit_generator::operator()(ctx); }
 
@@ -995,7 +997,8 @@ void jit_trans_ow_oc_t::generate() {
 struct jit_trans_iw_x4_4x_t : public jit_trans_src_t, public jit_generator {
     DECLARE_CPU_JIT_AUX_FUNCTIONS(jit_trans_iw_x4_4x_t)
 
-    jit_trans_iw_x4_4x_t(const jit_conv_conf_t *conf) : jit_trans_src_t(conf) {}
+    jit_trans_iw_x4_4x_t(const jit_conv_conf_t *conf)
+        : jit_trans_src_t(conf), jit_generator(jit_name()) {}
 
     void generate() override;
 
